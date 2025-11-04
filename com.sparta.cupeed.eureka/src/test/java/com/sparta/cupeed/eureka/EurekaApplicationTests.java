ceb21c7ad6fd108d845229715dbe575719ff0918
River is line <dmstjs7047@hanmail.net>
2025-11-04 16:46:58 +0900

    setting : eureka server project setting

---
+++ com.sparta.cupeed.eureka/src/test/java/com/sparta/cupeed/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cupeed.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
