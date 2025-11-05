060127522c97925f63afabd00e1444c457b3087e
River is line <dmstjs7047@hanmail.net>
2025-11-05 11:51:37 +0900

    setting : msa service setting

---
+++ com.sparta.cupeed.hubroute/src/main/java/com/sparta/cupeed/hubroute/HubrouteApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cupeed.hubroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubrouteApplication.class, args);
+	}
+
+}
