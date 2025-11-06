fee37f283c01525f48d83c1ee8e51feb8fed3b23
believeme <cain1227@naver.com>
2025-11-06 15:18:46 +0900

    feat: #18 상품생성 필요없는 파일 삭제 및 레포지토리 파일 분리

--- com.sparta.cupeed.product/src/main/java/com/sparta/cupeed/product/infrastructure/repository/ProductJpaRepository.java
+++ com.sparta.cupeed.product/src/main/java/com/sparta/cupeed/product/infrastructure/repository/ProductJpaRepository.java
@@ -1,9 +1,11 @@
-// package com.sparta.cupeed.product.infrastructure.repository;
-//
-// import java.util.UUID;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.sparta.cupeed.product.domain.entity.Product;
-//
-// public interface ProductJpaRepository extends JpaRepository<Product, UUID> {
-// 	boolean existsByName(String name);
-// }
+package com.sparta.cupeed.product.infrastructure.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sparta.cupeed.product.domain.entity.Product;
+
+@Repository
+public interface ProductJpaRepository extends JpaRepository<Product, UUID> {
+	boolean existsByName(String name);
+}
