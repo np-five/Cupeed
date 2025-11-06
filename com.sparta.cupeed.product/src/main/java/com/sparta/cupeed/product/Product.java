e64539f0f3f2c8dd6f1a27ae96622f5f8b667aee
believeme <cain1227@naver.com>
2025-11-06 11:43:42 +0900

    feat: #13 데이터 통신 확인

---
+++ com.sparta.cupeed.product/src/main/java/com/sparta/cupeed/product/Product.java
@@ -0,0 +1,17 @@
+package com.sparta.cupeed.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+	private String name;
+	private String description;
+
+}
