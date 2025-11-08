8a4bd0d7df5f8cecee7913c796a741d780a1a472
River is line <dmstjs7047@hanmail.net>
2025-11-09 03:58:12 +0900

    fix : request body의 내부 field가 인식되지 않는 오류 해결
    field 선언을 {}가 아닌 ()의 내부에 선언함.

--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthSignUpCommand.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthSignUpCommand.java
@@ -2,20 +2,21 @@ package com.sparta.cupeed.user.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record AuthSignUpCommand() {
+public record AuthSignUpCommand(
 
 	@Schema(description = "아이디", example = "cupeed123")
-	private static String id;
+	String id,
 
 	@Schema(description = "비밀번호", example = "cupeed!234")
-	private static String password;
+	String password,
 
 	@Schema(description = "슬랙 아이디", example = "cupeed")
-	private static String slackId;
+	String slackId,
 
 	@Schema(description = "회사 이름", example = "스파르타코딩클럽")
-	private static String companyName;
+	String companyName,
 
 	@Schema(description = "사업자 등록 번호", example = "123-45-67890")
-	private static String businessNo;
+	String businessNo
+) {
 }
