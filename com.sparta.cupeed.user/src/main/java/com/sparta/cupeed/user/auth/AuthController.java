56bb349033c74495687114b78d4459f19d612331
River is line <dmstjs7047@hanmail.net>
2025-11-09 03:49:31 +0900

    docs : swagger 문서 작성

--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthController.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/v1/auth")
+@Tag(name = "인증 API", description = "인증 관련 기능 API입니다.")
 public class AuthController {
 
 	@Operation(summary = "회원 가입", description = "회원 가입 api입니다.")
--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthSignUpCommand.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/AuthSignUpCommand.java
@@ -1,10 +1,21 @@
 package com.sparta.cupeed.user.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AuthSignUpCommand() {
 
+	@Schema(description = "아이디", example = "cupeed123")
 	private static String id;
+
+	@Schema(description = "비밀번호", example = "cupeed!234")
 	private static String password;
+
+	@Schema(description = "슬랙 아이디", example = "cupeed")
 	private static String slackId;
+
+	@Schema(description = "회사 이름", example = "스파르타코딩클럽")
 	private static String companyName;
+
+	@Schema(description = "사업자 등록 번호", example = "123-45-67890")
 	private static String businessNo;
 }
