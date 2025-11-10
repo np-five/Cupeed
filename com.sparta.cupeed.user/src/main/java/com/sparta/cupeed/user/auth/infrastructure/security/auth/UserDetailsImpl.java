ce13d7bcefb4b31dfc9b031b2fb03442b01a911c
River is line <dmstjs7047@hanmail.net>
2025-11-11 02:36:26 +0900

    feat : Authorization 필터 등록

---
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/infrastructure/security/auth/UserDetailsImpl.java
@@ -0,0 +1,39 @@
+package com.sparta.cupeed.user.auth.infrastructure.security.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.sparta.cupeed.user.auth.infrastructure.jpa.entity.RoleEnum;
+import com.sparta.cupeed.user.auth.infrastructure.jpa.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+	private final UserEntity userEntity;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public String getPassword() {
+		return userEntity.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return userEntity.getUserId();
+	}
+
+	public RoleEnum getRole() {
+		return userEntity.getRole();
+	}
+}
---
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/infrastructure/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,71 @@
+package com.sparta.cupeed.user.auth.infrastructure.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.sparta.cupeed.user.auth.infrastructure.security.auth.UserDetailsServiceImpl;
+import com.sparta.cupeed.user.auth.infrastructure.security.jwt.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtil;
+	private final UserDetailsServiceImpl userDetailsService;
+
+	@Override
+	protected void doFilterInternal(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain
+	) throws ServletException, IOException {
+		String token = jwtUtil.getJwtTokenFromHeader(request);
+
+		if (!StringUtils.hasText(token)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		try {
+			if (!jwtUtil.validateToken(token)) {
+				response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid JWT Token");
+				return;
+			}
+
+			String userId = jwtUtil.getUserInfoFromToken(token).getSubject();
+			setAuthentication(userId);
+		} catch (Exception e) {
+			SecurityContextHolder.clearContext();
+			response.sendError(HttpServletResponse.SC_FORBIDDEN, "Authentication failed");
+			return;
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	public void setAuthentication(String userId) {
+		SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+		UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(
+			userDetails,
+			null,
+			userDetails.getAuthorities());
+
+		securityContext.setAuthentication(authentication);
+		SecurityContextHolder.setContext(securityContext);
+	}
+}
