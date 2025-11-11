cdd1eef99ccf51a8d79e42778a9355e4b75dd6a4
River is line <dmstjs7047@hanmail.net>
2025-11-11 15:27:23 +0900

    feat : 인가 구현

--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/infrastructure/security/auth/UserDetailsImpl.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/auth/infrastructure/security/auth/UserDetailsImpl.java
@@ -1,11 +1,13 @@
 package com.sparta.cupeed.user.auth.infrastructure.security.auth;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.sparta.cupeed.user.auth.infrastructure.jpa.entity.RoleEnum;
 import com.sparta.cupeed.user.auth.infrastructure.jpa.entity.UserEntity;
 
 import lombok.Getter;
@@ -19,8 +21,11 @@ public class UserDetailsImpl implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO: 인가 구현
-		return new ArrayList<>();
+		RoleEnum role = userEntity.getRole();
+		String authority = role.getAuthority();
+		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+
+		return List.of(simpleGrantedAuthority);
 	}
 
 	@Override
