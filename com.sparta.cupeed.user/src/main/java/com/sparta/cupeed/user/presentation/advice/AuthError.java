3f780c81db4286ba16a9d561d0f0db4aabdafb96
River is line <dmstjs7047@hanmail.net>
2025-11-12 04:40:43 +0900

    feat : 입력값 검증 로직 추가

--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/presentation/advice/AuthError.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/presentation/advice/AuthError.java
@@ -13,6 +13,7 @@ public enum AuthError {
 	INTERNAL_SERVER_ERROR("AU000", "서버 내부 오류입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 	ACCESS_DENIED("AU001", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
 	AUTH_USER_NOT_FOUND("AU002", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+	INVALID_INPUT("AU003", "입력값이 잘못되었습니다.", HttpStatus.BAD_REQUEST),
 
 	// 회원가입
 	AUTH_USER_ID_ALREADY_EXISTS("AU010", "이미 존재하는 아이디입니다.", HttpStatus.BAD_REQUEST),
--- com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/presentation/advice/AuthExceptionHandler.java
+++ com.sparta.cupeed.user/src/main/java/com/sparta/cupeed/user/presentation/advice/AuthExceptionHandler.java
@@ -2,9 +2,15 @@ package com.sparta.cupeed.user.presentation.advice;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
@@ -26,6 +32,23 @@ public class AuthExceptionHandler {
 			"AuthException occurred: code={}, message={}",
 			authError.getCode(),
 			authError.getMessage() + '\n' + ex.getMessage());
+
+		return ResponseEntity.status(authError.getStatus()).body(ApiResponse.error(authError));
+	}
+
+	// 입력값 검증
+	@ExceptionHandler({
+		MethodArgumentNotValidException.class,
+		MethodArgumentTypeMismatchException.class,
+		MissingPathVariableException.class,
+		MissingServletRequestParameterException.class,
+		ConstraintViolationException.class,
+		BindException.class
+	})
+	public ResponseEntity<ApiResponse<Void>> handleValidation(Exception exception) {
+		AuthError authError = AuthError.INVALID_INPUT;
+
+		log.error("[{}] {}", authError.getCode(), authError.getMessage(), exception);
 
 		return ResponseEntity.status(authError.getStatus()).body(ApiResponse.error(authError));
 	}
