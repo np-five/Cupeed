450fdb6f56bf131c41c39bb9fb3c0af5c802e1b2
hohohosn <day7of01@gmail.com>
2025-11-13 08:45:29 +0900

    fix: 코드컨벤션 준수

--- com.sparta.cupeed.delivery/src/main/java/com/sparta/cupeed/delivery/presentation/controller/DeliveryRouteControllerV1.java
+++ com.sparta.cupeed.delivery/src/main/java/com/sparta/cupeed/delivery/presentation/controller/DeliveryRouteControllerV1.java
@@ -19,7 +19,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/deliveries/{deliveryId}/routes")
+@RequestMapping("/v1/deliveries/{deliveryId}/routes")
 @RequiredArgsConstructor
 public class DeliveryRouteControllerV1 {
 
