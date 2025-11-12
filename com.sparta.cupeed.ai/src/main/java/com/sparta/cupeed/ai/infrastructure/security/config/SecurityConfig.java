a58b85c4a251031b70a7baa57ca3804fd19c082e
chachohee <cxoxee@gmail.com>
2025-11-12 21:19:52 +0900

    feat : security 추가

--- com.sparta.cupeed.ai/src/main/java/com/sparta/cupeed/ai/infrastructure/security/config/SecurityConfig.java
+++ com.sparta.cupeed.ai/src/main/java/com/sparta/cupeed/ai/infrastructure/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sparta.cupeed.slack.infrastructure.security.config;
+package com.sparta.cupeed.ai.infrastructure.security.config;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import com.sparta.cupeed.slack.infrastructure.security.filter.JwtAuthorizationFilter;
+import com.sparta.cupeed.ai.infrastructure.security.filter.JwtAuthorizationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -47,17 +47,17 @@ public class SecurityConfig {
 			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 			.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(
-					"/swagger-ui/**",
-					"/swagger",
-					"/v3/api-docs/**",
-					"/v3/api-docs.yaml",
-					"/actuator/**"
-				).permitAll()
-				.anyRequest().authenticated()
+				// .requestMatchers(
+				// 	"/swagger-ui/**",
+				// 	"/swagger",
+				// 	"/v3/api-docs/**",
+				// 	"/v3/api-docs.yaml",
+				// 	"/actuator/**"
+				// ).permitAll()
+				// .anyRequest().authenticated()
 
-				// 시큐리티 적용 안하고 테스트
-				// .anyRequest().permitAll()
+				// 시큐리티 적용 안하고 테스트중
+				.anyRequest().permitAll()
 			);
 
 		return http.build();
