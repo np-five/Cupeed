e89db00c79a446a196154af277722241f8d31235
hwangtae <htk5205@naver.com>
2025-11-13 12:23:40 +0900

    [fix] 안쓰는 변수, 메서드 제거

--- com.sparta.cupeed.hubroute/src/main/java/com/sparta/cupeed/hubroute/application/HubRouteService.java
+++ com.sparta.cupeed.hubroute/src/main/java/com/sparta/cupeed/hubroute/application/HubRouteService.java
@@ -27,8 +27,9 @@ public class HubRouteService {
 	private final HubClient hubClient;
 	private final NaverMapClient naverMapClient;
 
+	// 1. 허브 라우트 생성 (POST /v1/hub-routes)
 	@Transactional
-	public HubRouteResponse createDynamicHubRoute(UUID startHubId, UUID endHubId) {
+	public HubRouteResponse createHubRoute(UUID startHubId, UUID endHubId) {
 		// 출발지/도착지 Hub 검증 (Feign Client 사용)
 		validateHubs(startHubId, endHubId);
 
@@ -60,40 +61,22 @@ public class HubRouteService {
 		return HubRouteResponse.mapToResponse(newRoute);
 	}
 
-	// 1. 허브 라우트 생성 (POST /v1/hub-routes)
-	// public HubRouteResponse createHubRoute(CreateHubRouteCommand request) {
-	//
-	// 	// 출발지/도착지 Hub 검증 (Feign Client 사용)
-	// 	validateHubs(request.getStartHubId(), request.getEndHubId());
-	//
-	// 	HubRoute newRoute = new HubRoute(
-	// 		request.getStartHubId(),
-	// 		request.getEndHubId(),
-	// 		request.getDuration(),
-	// 		request.getDistance()
-	// 	);
-	//
-	// 	HubRoute savedRoute = hubRouteRepository.save(newRoute);
-	// 	return HubRouteResponse.mapToResponse(savedRoute);
-	// }
-
 	// 2. 허브 라우트 목록 조회 (GET /v1/hub-routes)
-
 	public List<HubRouteResponse> findAllHubRoutes() {
 		return hubRouteRepository.findAll().stream()
 			.map(HubRouteResponse::mapToResponse)
 			.collect(Collectors.toList());
 	}
-	// 3. 허브 라우트 단건 조회 (GET /v1/hub-routes/{hubRouteId})
 
+	// 3. 허브 라우트 단건 조회 (GET /v1/hub-routes/{hubRouteId})
 	public HubRouteResponse findHubRouteById(UUID id) {
 		HubRoute route = hubRouteRepository.findById(id)
 			.orElseThrow(() -> new IllegalArgumentException("Hub Route ID를 찾을 수 없습니다"));
 
 		return HubRouteResponse.mapToResponse(route);
 	}
-	// 4. 허브 라우트 수정 (PUT /v1/hub-routes/{hubRouteId})
 
+	// 4. 허브 라우트 수정 (PUT /v1/hub-routes/{hubRouteId})
 	@Transactional
 	public HubRouteResponse updateHubRoute(UUID id, UpdateHubRouteCommand request) {
 		HubRoute route = hubRouteRepository.findById(id)
@@ -115,8 +98,8 @@ public class HubRouteService {
 
 		return HubRouteResponse.mapToResponse(route);
 	}
-	// 5. 허브 라우트 삭제 (DELETE /v1/hub-routes/{hubRouteId})
 
+	// 5. 허브 라우트 삭제 (DELETE /v1/hub-routes/{hubRouteId})
 	@Transactional
 	public void deleteHubRoute(UUID id) {
 		HubRoute route = hubRouteRepository.findById(id)
--- com.sparta.cupeed.hubroute/src/main/java/com/sparta/cupeed/hubroute/application/command/CreateHubRouteCommand.java
+++ com.sparta.cupeed.hubroute/src/main/java/com/sparta/cupeed/hubroute/application/command/CreateHubRouteCommand.java
@@ -12,12 +12,4 @@ public class CreateHubRouteCommand {
 
 	@NotNull(message = "도착 Hub ID는 필수입니다.")
 	UUID endHubId;
-
-	// @NotNull(message = "소요 시간은 필수입니다.")
-	// @Min(value = 0, message = "소요 시간은 0시간 이상이어야 합니다.")
-	// Double duration;
-	//
-	// @NotNull(message = "거리는 필수입니다.")
-	// @Min(value = 0, message = "거리는 0km 이상이어야 합니다.")
-	// Double distance;
 }
